843b762592b00ced9788970795745437fdcefd81
Aline <p.aline.vanderklok@hotmail.com>
2019-05-20 13:25:19 +0200

    changed datum opdr (2.a)

--- Datum.java
+++ Datum.java
@@ -4,16 +4,26 @@ public class Datum {
 	private int maand;
 	private int jaar;
 
-	  /**
-	   * Constructor
-	   */
-	// TODO
-	
+	/**
+	 * Constructor
+	 */
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
+
+	public Datum() {
+		dag = 0;
+		maand = 0;
+		jaar = 0;
+	}
+
 	public boolean bestaatDatum(int dag, int maand, int jaar){
 		// TODO
 		return false;
 	}
-	
+
 	/**
 	 * Getter voor Sting weergave van datum
 	 * @return Geboortedatum
@@ -21,5 +31,51 @@ public class Datum {
 	public String getDatumAsString() {
 		// TODO
 		return "";
-		}
+	}
+
+	/**
+	 * getter voor de int dag
+	 * @return Dag als int
+	 */
+	public int getDag() {
+		return dag;
+	}
+
+	/**
+	 * getter voor de int maand
+	 * @return Maand als int
+	 */
+	public int getMaand() {
+		return maand;
+	}
+
+	/**
+	 * getter voor de int jaar
+	 * @return Jaar als int
+	 */
+	public int getJaar() {
+		return jaar;
+	}
+
+	/**
+	 * setter voor de dag int
+	 */
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	/**
+	 * setter voor de maand int
+	 */
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	/**
+	 * setter voor de jaar int
+	 */
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
 }
