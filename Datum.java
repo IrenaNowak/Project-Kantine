59d111aad804178dcecdb149821049e94ebed94b
Aline <p.aline.vanderklok@hotmail.com>
2019-05-26 11:05:45 +0200

    opdr 2 en opdr 5 gemaakt

--- Datum.java
+++ Datum.java
@@ -5,23 +5,25 @@ public class Datum {
 	private int jaar;
 
 	/**
-	 * Constructor
+	 * Constructor met parameters dag, maand en jaar.
+	 * roept eerst de parameterloze constructor op met this()
 	 */
 	public Datum(int dag, int maand, int jaar) {
-		this.dag = dag;
-		this.maand = maand;
-		this.jaar = jaar;
+		this();
+		if(bestaatDatum(dag, maand, jaar)) {
+			this.dag = dag;
+			this.maand = maand;
+			this.jaar = jaar;
+		}
 	}
 
+	/**
+	 * Constructor zonder parameters
+	 */
 	public Datum() {
 		dag = 0;
 		maand = 0;
 		jaar = 0;
-	}
-
-	public boolean bestaatDatum(int dag, int maand, int jaar){
-		// TODO
-		return false;
 	}
 
 	/**
@@ -59,6 +61,7 @@ public class Datum {
 
 	/**
 	 * setter voor de dag int
+	 * @param int dag voor de dag
 	 */
 	public void setDag(int dag) {
 		this.dag = dag;
@@ -66,6 +69,7 @@ public class Datum {
 
 	/**
 	 * setter voor de maand int
+	 * @param int maand voor de maand
 	 */
 	public void setMaand(int maand) {
 		this.maand = maand;
@@ -73,9 +77,45 @@ public class Datum {
 
 	/**
 	 * setter voor de jaar int
+	 * @param int jaar voor het jaar
 	 */
 	public void setJaar(int jaar) {
 		this.jaar = jaar;
 	}
 
+	/**
+	 * checkt of de datum wel geldig is
+	 * @return
+	 */
+	public boolean bestaatDatum(int dag, int maand, int jaar) {
+		int[] maanden31D = {1, 3, 5, 7, 8, 10, 12};
+		if(dag >= 1 && maand >= 1 && maand <= 12 && jaar >= 1900 && jaar <= 2100) {
+			for(int x : maanden31D) {
+				if (x == maand) {
+					if(dag <= 31) {
+						return true;
+					}
+				}
+				else if (maand == 2) {
+					if((jaar % 4) == 0 ) {
+						if(dag <= 29){
+							return true;
+						}
+					}
+					else{
+						if(dag <= 28) {
+							return true;
+						}
+
+					}
+				}
+				else {
+					if (dag <= 30) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
--- KassaRij.java
+++ KassaRij.java
@@ -3,20 +3,21 @@ import java.util.LinkedList;
 
 public class KassaRij {
 
+    private ArrayList<Dienblad> rij;
+
     /**
      * Constructor
      */
     public KassaRij() {
-        // method body omitted
+        rij = new ArrayList<>();
     }
 
     /**
      * Persoon sluit achter in de rij aan
-     *
      * @param klant
      */
     public void sluitAchteraan(Dienblad klant) {
-        // method body omitted
+        rij.add(klant);
     }
 
     /**
@@ -24,18 +25,26 @@ public class KassaRij {
      * de rij verwijderen en retourneren.
      * Als er niemand in de rij staat geeft deze null terug.
      *
-     * @return Eerste klant in de rij of null
+     * @return null als er geen klanten zijn
+     *          verwijdering eerste van de rij als er wel klanten zijn
      */
     public Dienblad eerstePersoonInRij() {
-        // method body omitted
+        if(erIsEenRij() == true) {
+            rij.remove(0);
+        }
+        return null;
     }
 
     /**
      * Methode kijkt of er personen in de rij staan.
      *
-     * @return Of er wel of geen rij bestaat
+     * @return false als er geen rij is
+     *          true als er wel een rij is
      */
     public boolean erIsEenRij() {
-        // method body omitted
+        if(rij.size() == 0) {
+            return false;
+        }
+        else return true;
     }
 }
