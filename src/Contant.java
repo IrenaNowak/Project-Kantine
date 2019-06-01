d9df26f3f5474fa9f633679406c832e5f1b34bff
Irena <ghostirena@gmail.com>
2019-06-01 21:50:15 +0200

    testclass voor bestaatDatum() method met junit 5 toegevoegd [moet laten zien aan leraar volgende x]

--- src/Contant.java
+++ src/Contant.java
@@ -5,6 +5,7 @@ public class Contant extends Betaalwijze {
      * Methode om betaling af te handelen
      */
     public boolean betaal(double tebetalen) {
-        // method body omitted
+        return false;
+        // todo
     }
 }
--- src/Datum.java
+++ src/Datum.java
@@ -34,7 +34,6 @@ public class Datum {
      * @return String van de datum
      */
     public String getDatumAsString() {
-        // TODO
         return String.format("{0:D2}-{1:D2}-{2}", dag, maand, jaar);
     }
 
@@ -97,7 +96,7 @@ public class Datum {
      *
      * @return true or false wanneer de datum wel of niet bestaat
      */
-    public boolean bestaatDatum(int dag, int maand, int jaar) {
+    public static boolean bestaatDatum(int dag, int maand, int jaar) {
         int[] maanden31D = {1, 3, 5, 7, 8, 10, 12};
         if (dag >= 1 && maand >= 1 && maand <= 12 && jaar >= 1900 && jaar <= 2100) {
             for (int x : maanden31D) {
